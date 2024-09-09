df73e2ac505b04b2bb332b000d4acf5d5c2373e8
Robert Haughton <robert.haughton@mail.utoronto.ca>
2024-09-09 17:53:33 -0400

    Changed access modifiers using Intellij

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -11,7 +11,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int j) {
+    public static int doFizzBuzz(int j) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = j % 3 == 0;
         boolean divisibleBy5 = j % 5 == 0;
